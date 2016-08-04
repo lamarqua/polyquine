0fb0b761b35927116cd4b85f1d5dab5353c0dd6b
Adrien Lamarque <ad@lamarque.fr>
2016-08-04 16:49:03 -0400

    WOOT

--- convert.py
+++ convert.py
@@ -2,7 +2,10 @@ import sys
 
 with open(sys.argv[1]) as f:
 	s = f.read()
-	s = s[:s.find("quineData = \"")]
+	pattern = "quineData = \""
+	if len(sys.argv) >= 2:
+		pattern = sys.argv[2]
+	s = s[:s.find(pattern)]
 	print(s.encode('hex'))
 
 ## cut
